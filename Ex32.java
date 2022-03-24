23daaca513ba7086177bf325a2f4306d718853d4
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-24 00:06:50 -0300

    Ex32 feito

---
+++ Ex32.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Ex32{
+    public static void main(String[] args) {
+        Random gerador = new Random();
+        Scanner leitor = new Scanner (System.in);
+        System.out.println("Qual o valor de n");
+        int n = leitor.nextInt();
+        for (int i = 1; i <= n; i++){
+            int d1 = gerador.nextInt(6) + 1;
+            int d2 = gerador.nextInt(6) + 1;
+            System.out.printf("%d %c %d\n", d1, d1 > d2 ? '>' : d2 > d1 ? '<': '=', d2);
+        }
+        leitor.close();        
+    }
+}
